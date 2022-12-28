9261ece071b5fdf926fe7771d8a3af9f91759166
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-28 21:14:01 +0530

    Create Rotate_Array.cpp

---
+++ Array/Rotate Array/Rotate_Array.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(int arr[], int d, int n)
+    {
+        if (d > n)
+            d = d % n;
+        reverse(arr, arr + d);
+        reverse(arr + d, arr + n);
+        reverse(arr, arr + n);
+    }
+};
