29c84d2b25acd47abffcb6551ca7d54e88f39599
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-28 21:17:29 +0530

    Create Search_an_Element_in_an_array.cpp

---
+++ Array/Search an Element in an array/Search_an_Element_in_an_array.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int n, int x)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+};
