d6e4d52084d5087428dddf484d8f1568c623d4e7
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-28 18:52:20 +0530

    array problems added

---
+++ Array/Leaders in an array/Leaders_in_an_array.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    // Function to find the leaders in the array.
+public:
+    vector<int> leaders(int a[], int n)
+    {
+        vector<int> ans;
+        ans.push_back(a[n - 1]);
+        int max = a[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (a[i] >= max)
+            {
+                max = a[i];
+                ans.push_back(a[i]);
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
---
+++ Array/Sort an array of 0s, 1s and 2s/sort_an_array_of_os1s_2s.cpp
@@ -0,0 +1,9 @@
+class Solution
+{
+public:
+    void sort012(int a[], int n)
+    {
+        // code here
+        sort(a, a + n);
+    }
+};
