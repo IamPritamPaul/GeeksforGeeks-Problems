e1055f3ab32042882d7bb84df4db8431e47dd7fa
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 16:57:59 +0530

    Create Swap_kth_elements.cpp

---
+++ Array/Swap kth elements/Swap_kth_elements.cpp
@@ -0,0 +1,11 @@
+// User function template for C++
+class Solution
+{
+public:
+    // swap k'th element from beginning and end
+    void swapKth(int *arr, int n, int k)
+    {
+        // code here
+        swap(arr[k - 1], arr[n - k]);
+    }
+};
