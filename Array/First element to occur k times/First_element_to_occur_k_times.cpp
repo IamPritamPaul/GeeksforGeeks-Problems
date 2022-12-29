88c4e8d5221d08d9d37ef9e966aadf232ac0d14b
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 12:48:20 +0530

    Create First_element_to_occur_k_times.cpp

---
+++ Array/First element to occur k times/First_element_to_occur_k_times.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int firstElementKTime(int a[], int n, int k)
+    {
+        map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[a[i]]++;
+            if (m[a[i]] == k)
+                return a[i];
+        }
+        return -1;
+    }
+};
