6bfe5aa0bd6ab9f682ea61fe1ded461b229adc0f
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 13:09:07 +0530

    Create Index_Of_an_Extra_Element.cpp

---
+++ Array/Index Of an Extra Element/Index_Of_an_Extra_Element.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int findExtra(int a[], int b[], int n)
+    {
+        // add code here.
+        int i, j;
+        for (i = 0, j = 0; i < n, j < n - 1; i++, j++)
+        {
+            if (a[i] != b[j])
+                return i;
+        }
+    }
+};
