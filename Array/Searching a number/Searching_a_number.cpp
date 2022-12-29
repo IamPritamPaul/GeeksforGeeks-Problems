ee6d597eb6b7984bbf69090cc702b9695a3d2754
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 13:09:13 +0530

    Create Searching_a_number.cpp

---
+++ Array/Searching a number/Searching_a_number.cpp
@@ -0,0 +1,14 @@
+// User function template for C++
+class Solution
+{
+public:
+    int search(int arr[], int n, int k)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == k)
+                return i + 1;
+        }
+        return -1;
+    }
+};
