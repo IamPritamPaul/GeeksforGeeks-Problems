e3e8f1b61ef7ee914460a700498a8c5694b7178c
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 13:09:10 +0530

    Create Move_all_zeroes_to_end_of_array.cpp

---
+++ Array/Move all zeroes to end of array/Move_all_zeroes_to_end_of_array.cpp
@@ -0,0 +1,17 @@
+// User function template for C++
+class Solution
+{
+public:
+    void pushZerosToEnd(int arr[], int n)
+    {
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] != 0)
+            {
+                swap(arr[i], arr[j]);
+                j++;
+            }
+        }
+    }
+};
