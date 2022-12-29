6d107274101ea54a09e318df54a2d61c50be19be
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 12:28:00 +0530

    Create Print_alternate_elements_of_an_array.cpp

---
+++ Array/Print alternate elements of an array/Print_alternate_elements_of_an_array.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+// ar[] is the array
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    for (int i = 0; i < n; i += 2)
+        cout << ar[i] << " ";
+}
