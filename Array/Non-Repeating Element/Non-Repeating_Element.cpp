b68f840200997fd641694e754f9999c6a9da766b
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 16:57:52 +0530

    Create Non-Repeating_Element.cpp

---
+++ Array/Non-Repeating Element/Non-Repeating_Element.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int firstNonRepeating(int arr[], int n)
+    {
+        int count = 0;
+        map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[arr[i]]++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (m[arr[i]] == 1)
+            {
+                return arr[i];
+            }
+        }
+        return 0;
+    }
+};
