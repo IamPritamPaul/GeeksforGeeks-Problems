ef200effe16741bb98c198db3ecf5634926bebaf
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 12:21:44 +0530

    Create First_Repeating_Element..cpp

---
+++ Array/First Repeating Element/First_Repeating_Element..cpp
@@ -0,0 +1,23 @@
+// User function template in C++
+
+class Solution
+{
+public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n)
+    {
+        map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[arr[i]]++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (m[arr[i]] > 1)
+            {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+};
