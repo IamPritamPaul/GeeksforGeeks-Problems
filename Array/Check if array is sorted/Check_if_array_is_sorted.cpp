e599c809338445bdc5d9518a2a78060271fe1d9b
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 13:09:02 +0530

    Create Check_if_array_is_sorted.cpp

---
+++ Array/Check if array is sorted/Check_if_array_is_sorted.cpp
@@ -0,0 +1,15 @@
+// User function template for C++
+
+class Solution
+{
+public:
+    bool arraySortedOrNot(int arr[], int n)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+                return false;
+        }
+        return true;
+    }
+};
