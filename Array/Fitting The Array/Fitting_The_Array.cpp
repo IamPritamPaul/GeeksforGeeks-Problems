e375821d0b5d2a136d0fac28e901ec3078abf2b1
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 15:38:33 +0530

    Create Fitting_The_Array.cpp

---
+++ Array/Fitting The Array/Fitting_The_Array.cpp
@@ -0,0 +1,17 @@
+// Back-end complete function Template for C++
+class Solution
+{
+public:
+    bool isFit(int arr[], int brr[], int n)
+    {
+        // code here
+        sort(arr, arr + n);
+        sort(brr, brr + n);
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > brr[i])
+                return false;
+        }
+        return true;
+    }
+};
