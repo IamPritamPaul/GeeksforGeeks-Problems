000fb0bb0d23ada8842f4387f0f211c1bcad87df
PRITAM PAUL <paul.pritamg3@gmail.com>
2022-12-29 16:57:54 +0530

    Create Print_Elements_of_Array.cpp

---
+++ Array/Print Elements of Array/Print_Elements_of_Array.cpp
@@ -0,0 +1,14 @@
+// User function template for C++
+class Solution
+{
+public:
+    // Just print the space seperated array elements
+    void printArray(int arr[], int n)
+    {
+        // code here
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+};
