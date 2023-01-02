19da9f36ae1b7daf35cc6701c1444e87ecbd85c0
PRITAM PAUL <paul.pritamg3@gmail.com>
2023-01-03 00:53:11 +0530

    Create Remove duplicates from an unsorted linked list.cpp

---
+++ Linked List/Remove duplicates from an unsorted linked list/Remove duplicates from an unsorted linked list.cpp
@@ -0,0 +1,39 @@
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution
+{
+public:
+    // Function to remove duplicates from unsorted linked list.
+    Node *removeDuplicates(Node *head)
+    {
+        unordered_set<int> s;
+        Node *curr = head;
+        Node *prev = NULL;
+        while (curr != NULL)
+        {
+            if (s.find(curr->data) != s.end())
+            {
+                prev->next = curr->next;
+                delete (curr);
+            }
+            else
+            {
+                s.insert(curr->data);
+                prev = curr;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
